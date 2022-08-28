808bfc515b20fac636d2f8d55a18d6ccecd0b479
OsmanBytyqi <obytyqi2001@gmail.com>
2022-08-28 13:36:15 +0200

    stuff

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,32 +1,27 @@
-const express=require("express")
-const Workout=require("../models/Workout")
-const router=express.Router();
-
+const express = require('express')
 const {
-    getWorkouts,
-    getWorkout,
-    createWorkout,
-    deleteWorkout,
-    updateWorkout
-} = require("../controllers/workoutControllers")
+  getWorkouts, 
+  getWorkout, 
+  createWorkout, 
+  deleteWorkout, 
+  updateWorkout
+} = require('../controllers/workoutController')
 
+const router = express.Router()
 
-// get all workouts
-router.get('/',getWorkouts);
+// GET all workouts
+router.get('/', getWorkouts)
 
-// get a single workout
-router.get('/:id',getWorkout);
+// GET a single workout
+router.get('/:id', getWorkout)
 
-// post a new workout
-router.post('/',createWorkout);
+// POST a new workout
+router.post('/', createWorkout)
 
-// delete a new workout
-router.delete("/:id",deleteWorkout)
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
 
-//update a workout
-router.patch("/:id",updateWorkout)
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
 
-
-
-module.exports=router
-
+module.exports = router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
-          <div className="pages">
-              <Routes>
-                  <Route path="/" element={<Home/>} />
-              </Routes>
-          </div>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
